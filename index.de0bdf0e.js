fd71df5a3b1b4ef987b5a85b2a7decb9cc35e1bd
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-03 23:37:05 +0300

    make build

---
+++ index.de0bdf0e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var a=t.target;"IMG"===a.tagName&&(a=a.parentElement),"A"===a.tagName&&(e.src=a.href)});
+//# sourceMappingURL=index.de0bdf0e.js.map
