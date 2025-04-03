cf7fbf67c7be67bf1148af8d8dc3d034450354fa
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-03 23:02:14 +0300

    add gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbnails = [...document.querySelectorAll('.list-item__link')];
+
+thumbnails.map((thumbnail) => {
+  thumbnail.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const pageUrl = document.URL.substring(0, document.URL.length - 1);
+    const newUrl = pageUrl + thumbnail.getAttribute('href');
+
+    mainImage.setAttribute('src', newUrl);
+  });
+});
